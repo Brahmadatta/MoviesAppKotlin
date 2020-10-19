d086d44efab76ab2790fb5eb5dae555d5966b97a
brahmadatta <g.brahmadatta@gmail.com>
2020-10-19 23:25:57 +0530

    added detail fragment data using data binding

--- app/src/main/java/com/example/myapplication/view/DetailFragment.kt
+++ app/src/main/java/com/example/myapplication/view/DetailFragment.kt
@@ -5,17 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentDetailBinding
+import com.example.myapplication.model.MovieModel
 
 class DetailFragment : Fragment() {
 
+    var movies : MovieModel ?= null
+
+    private lateinit var dataBinding : FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        //return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail,container,false)
+        return dataBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            movies = DetailFragmentArgs.fromBundle(it).movieDetailData
+        }
+
+        dataBinding.movies = movies
     }
 
 }
